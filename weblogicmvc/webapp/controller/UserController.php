375e9622fa1e8f3ace76339dc7ff6bf10cc2f2a1
Jason Mendes <jason_mendes_1998@hotmail.com>
2019-05-31 09:09:47 +0100

    yeaheeeeeeeeeeet

--- weblogicmvc/webapp/controller/UserController.php
+++ weblogicmvc/webapp/controller/UserController.php
@@ -5,14 +5,20 @@ use ArmoredCore\WebObjects\Redirect;
 use ArmoredCore\WebObjects\View;
 use ArmoredCore\Interfaces\ResourceControllerInterface;
 
+/**
+ * Created by PhpStorm.
+ * User: smendes
+ * Date: 08-04-2017
+ * Time: 12:36
+ */
 class UserController extends BaseController implements ResourceControllerInterface {
-        /**
+    /**
      * @return mixed
      */
     public function index()
     {
         $users = User::all();
-        View::make('users.index', ['users' => $users]);
+        View::make('user.index', ['user' => $users]);
 
     }
 
@@ -21,7 +27,7 @@ class UserController extends BaseController implements ResourceControllerInterfa
      */
     public function create()
     {
-        View::make('users.create');
+        View::make('user.create');
     }
 
     /**
@@ -31,14 +37,14 @@ class UserController extends BaseController implements ResourceControllerInterfa
     {
         // create new resource (activerecord/model) instance
         // your form name fields must match the ones of the table fields
-        $users = new Users(Post::getAll());
+        $users = new User(Post::getAll());
 
         if($users->is_valid()){
             $users->save();
-            Redirect::toRoute('users/index');
+            Redirect::toRoute('user/index');
         } else {
             // return form with data and errors
-            Redirect::flashToRoute('users/create', ['users' => $users]);
+            Redirect::flashToRoute('user/create', ['user' => $users]);
         }
     }
 
@@ -55,7 +61,7 @@ class UserController extends BaseController implements ResourceControllerInterfa
         if (is_null($users)) {
             // redirect to standard error page
         } else {
-            View::make('users.show', ['users' => $users]);
+            View::make('user.show', ['user' => $users]);
         }
     }
 
@@ -70,7 +76,7 @@ class UserController extends BaseController implements ResourceControllerInterfa
         if (is_null($users)) {
             // redirect to standard error page
         } else {
-            View::make('users.edit', ['users' => $users]);
+            View::make('user.edit', ['user' => $users]);
         }
     }
 
@@ -85,10 +91,10 @@ class UserController extends BaseController implements ResourceControllerInterfa
 
         if($users->is_valid()){
             $users->save();
-            Redirect::toRoute('users/index');
+            Redirect::toRoute('user/index');
         } else {
             // return form with data and errors
-            Redirect::flashToRoute('users/edit', ['users' => $users], $id);
+            Redirect::flashToRoute('user/edit', ['user' => $users], $id);
         }
     }
 
@@ -100,7 +106,6 @@ class UserController extends BaseController implements ResourceControllerInterfa
     {
         $users = User::find($id);
         $users->delete();
-        Redirect::toRoute('users/index');
+        Redirect::toRoute('user/index');
     }
-
 }
